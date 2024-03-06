0c3a84f08c9e33d88de8ba0bdcde410f0b35c654
naomi <naomina@me.com>
2024-03-06 13:40:56 +0900

    ミドルウェアの調整

--- src/pages/middleware.ts
+++ src/pages/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export const config = {
-  matcher: ['/:path*'],
+  matchers: ['/', '/index'],
 }
 
 export function middleware(req: NextRequest) {
